0c4cba050e2c1ed2df69c5caee14bcbbe02dfefd
Adilson Alves Mattos <adilson.day@hotmail.com>
2021-05-07 06:47:59 -0300

    muda o ip de conexão para o ip publico

--- src/main/java/AppSecrets.java
+++ src/main/java/AppSecrets.java
@@ -1,9 +1,9 @@
 
 public class AppSecrets {
 	// CHANGE
-	private final String url = "jdbc:postgresql://localhost:5432/t2s";
+	private final String url = "jdbc:postgresql://100.24.74.6:5432/t2s";
 	private final String dbUsername = "postgres";
-	private final String dbPassword = "ad123";
+	private final String dbPassword = "ad123456";
 	
 	
 	public String getUrl()
